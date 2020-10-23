52ecc62a251ee827eb126beab2db7b1b513ec097
Niyaz <niyazzif@gmail.com>
2020-10-23 16:25:51 +0500

    finalizing the little thing

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick();
+    $('.carousel__inner').slick({
+      speed: 1200,
+      adaptiveHeight: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_nav.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_nav.png"></button>'
+    });
   });
