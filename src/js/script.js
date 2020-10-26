08d402b3bc01bfe95bcd8e4e77b904fa7577340c
Niyaz <niyazzif@gmail.com>
2020-10-26 17:25:51 +0500

    add maskedinput

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,18 @@ $(document).ready(function(){
       speed: 1200,
       adaptiveHeight: true,
       prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_nav.png"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_nav.png"></button>'
+      nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_nav.png"></button>',
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            arrows: false,
+            dots: true
+            }
+        }
+      ]
     });
+    $('.feed-form').validate();
+    $('input[name=phone]').mask("+7 (999) 999-9999");
+
   });
